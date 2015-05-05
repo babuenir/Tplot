052762799c0a5dd77e314110c3c7acc6a261def3
BabuSubashChandar C <babusubashchandar@zilogic.com>
2015-05-05 17:52:41 +0530

    Removing the main function

--- tplotClient.py
+++ tplotClient.py
@@ -8,28 +8,3 @@ def connect_to_server(host, port):
     client.connect((host, port))
 
     return client
-
-
-def main():
-    # get local machine name
-    host = socket.gethostname()
-    port = 9999
-
-    cli = connect_to_server(host, port)
-
-    while True:
-       # Receive no more than 1024 bytes
-       data = cli.recv(1024)
-       reply = 'OK'
-
-       if not data:
-           break
-
-       print data
-       #cli.sendall(reply)
-
-    cli.close()
-
-
-if __name__ == "__main__":
-    main()
